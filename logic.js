9cce7c2e5b5b97883d841bd6ff6b135d2d02e853
Hambonzo <ledham@outlook.com>
2025-12-11 15:25:08 +0000

    Completed all requested features
    added prototypes for reading/changing boolean read
    added a data attr to del button so it can delete
    row and book object
    made new book pull up dialog
    close button closes dialog
    made submit send form values to js to make book
    edited display func to use del and change

--- logic.js
+++ logic.js
@@ -8,8 +8,16 @@ function Book(title, author, pages, finishedReading) {
     this.finishedReading = finishedReading;
 }
 
-Book.prototype.info = function() {
-     return this.title + " by " + this.author + ", " + this.pages + " pages, " + " " + this.finishedReading;
+Book.prototype.info = function () {
+    return this.title + " by " + this.author + ", " + this.pages + " pages, " + " " + this.finishedReading;
+}
+
+Book.prototype.read = function () {
+    return this.finishedReading ? "Yes" : "No";
+}
+
+Book.prototype.change = function () {
+    this.finishedReading = !this.finishedReading;
 }
 
 function addBookToLIbrary(title, author, pages, finishedReading) {
@@ -17,36 +25,84 @@ function addBookToLIbrary(title, author, pages, finishedReading) {
     let id = crypto.randomUUID();
     book.id = id;
     myLibrary.push(book);
-    
+
 }
 
-function displayLibrary(){
+function displayLibrary() {
 
     const tableBody = document.getElementById("library-body");
-    
     tableBody.innerHTML = "";
 
-    for(let Book of myLibrary) {
+    for (let Book of myLibrary) {
         let row = document.createElement("tr")
-        
+
         row.innerHTML = `
         <td>${Book.title}</td>
         <td>${Book.author}</td>
         <td>${Book.pages}</td>
-        <td>${Book.finishedReading}</td>
-        <td>${Book.id}</td>
+        <td class="status"><text>${Book.read()}</text><button class="change">Change</button></td>
+        <td><button class="del" data-id="${Book.id}">Delete</button></td>
         `
 
         tableBody.appendChild(row);
 
+        row.querySelector(".change").addEventListener("click", event => {
+            Book.change();
+            displayLibrary();
+        })
+
+        row.querySelector(".del").addEventListener("click", (event) => {
+            let id = event.target.dataset.id;
+
+            for (let i = 0; i < myLibrary.length; i++) {
+                if (myLibrary[i].id === id) {
+                    myLibrary.splice(i, 1);
+                    break;
+                }
+            }
+
+            // Remove the row from the table
+            event.target.parentElement.parentElement.remove();
+        });
+
     }
-}
+};
+
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("dialog + button");
+const closeButton = document.querySelector("#close");
+const submitButton = document.querySelector("#submit");
+const form = document.querySelector("form");
 
 
-addBookToLIbrary("The Fellowship of the ring", "Tolkien", 423, "Yes");
+showButton.addEventListener("click", () => {
+    dialog.showModal();
+});
 
-addBookToLIbrary("Words Of Radiance", "Brandon Sanderson", 1087, "Yes");
+closeButton.addEventListener("click", () => {
+    dialog.close();
+});
 
-addBookToLIbrary("War and Peace", "Leo Tolstoy", 1225, "No");
+
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let title = form.elements["title"].value;
+    let author = form.elements["author"].value;
+    let pages = form.elements["pages"].value;
+    
+    let readValue = form.elements["read"].value;
+    let finishedReading = readValue === "true";
+
+    addBookToLIbrary(title, author, pages, readValue);
+    displayLibrary();
+});
+
+
+addBookToLIbrary("The Fellowship of the ring", "Tolkien", 423, true);
+
+addBookToLIbrary("Words Of Radiance", "Brandon Sanderson", 1087, true);
+
+addBookToLIbrary("War and Peace", "Leo Tolstoy", 1225, false);
 
 displayLibrary();
